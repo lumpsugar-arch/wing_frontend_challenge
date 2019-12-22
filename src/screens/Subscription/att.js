96a4667cc7edac0e02df3bc16683686cf199bb29
Roman Afanasyev <afanasyev.r@yandex.ru>
2019-12-22 22:36:58 +0300

    Refactor subscription components

--- src/screens/Subscription/att.js
+++ src/screens/Subscription/att.js
@@ -1,79 +1,72 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { fetchFiltered as fetchFilteredInsuranceContracts } from 'insurance/insuranceContracts/actions';
+import { getFilteredInsuranceContracts, getAttSubscription } from 'reducers';
 import SubscriptionSwitcher from 'subscriptions/SubscriptionSwitcher';
 import Box from 'common/Box';
 import Link from 'common/Link';
-import Button from 'forms/Button';
 import * as routes from 'app/routes';
-import cn from 'classnames';
 import add_green_circle from 'common/img/add_green_circle.svg';
 import styles from './Subscription.module.css';
-import { fetchFiltered } from '../../insurance/insuranceContracts/api';
-import { getAttSubscription } from 'reducers';
 
-export const SubscriptionScreen = ({ subId, subscription }) => {
+export const SubscriptionScreen = (props) => {
   const [showInsuranceLink, setShowInsuranceLink] = useState(false);
-  const [showPlanLink, setShowPlanLink] = useState(false);
   const [sku, setSku] = useState('');
+  const { subId, fetchFilteredInsuranceContracts, contracts, subscription } = props;
 
   useEffect(() => {
-    fetchFiltered({subscription: subId}).then(res => {
-      if (res.data && res.data.count > 0) {
-        setShowInsuranceLink(false);
-        setShowPlanLink(false)
-      } else {
-        setShowInsuranceLink(true);
-      }
-    });
-  }, [subId]);
+    subId && fetchFilteredInsuranceContracts({ subscription: subId });
+  }, []);
 
   useEffect(() => {
+    if (!contracts) return;
+    if (contracts.length > 0) {
+      setShowInsuranceLink(false)
+    } else {
+      setShowInsuranceLink(true)
+    }
+
     if (!subscription) return;
-
     setSku(subscription.device_specs);
 
     const subStatus = subscription.att_status || subscription.sprint_status;
 
     if (subStatus === 'active') {
-      setShowPlanLink(false);
+      setShowInsuranceLink(false)
     } else {
-      setShowPlanLink(true)
+      setShowInsuranceLink(true)
     }
-  }, [subscription]);
+  }, [contracts]);
 
   return (
     <div className="Subscription">
       <SubscriptionSwitcher attSubId={subId} attRoute={routes.attSubscription} sprintRoute={routes.sprintSubscription} />
       <div className={styles.linkList}>
-        <Link
-          className={cn(
-            styles.subscriptionLink,
-            showInsuranceLink ? '' : styles.hidden
-          )}
-          to={routes.attInsurance(subId)}
-        >
-          <Box>
-            <img src={add_green_circle} />
-            Insurance
-          </Box>
-        </Link>
-        <Link
-          className={cn(showPlanLink ? '' : styles.hidden)}
-          to={routes.attInsurancePlan(subId, sku)}
-        >
-          <div className={styles.buttonChoose}>
-            <Button>Choose insurance plan</Button>
-          </div>
-        </Link>
+        {
+          showInsuranceLink ?
+            <Link className={styles.subscriptionLink} to={routes.attInsurancePlan(subId, sku)}>
+              <Box>
+                <img src={add_green_circle} />
+                Insurance
+              </Box>
+            </Link>
+            : null
+        }
       </div>
     </div>
   )
 };
 
 const mapStateToProps = (state, { subId }) => ({
-  subscription: getAttSubscription(state, subId)
+  subscription: getAttSubscription(state, subId),
+  contracts: getFilteredInsuranceContracts(state, {subscription: subId})
 });
 
+const mapDispatchToProps = {
+  fetchFilteredInsuranceContracts
+};
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(SubscriptionScreen)
--- src/screens/Subscription/sprint.js
+++ src/screens/Subscription/sprint.js
@@ -1,80 +1,73 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
+import { fetchFiltered as fetchFilteredInsuranceContracts } from 'insurance/insuranceContracts/actions';
+import { getFilteredInsuranceContracts, getSprintSubscription } from 'reducers';
 import SubscriptionSwitcher from 'subscriptions/SubscriptionSwitcher';
 import Box from 'common/Box';
 import Link from 'common/Link';
-import Button from 'forms/Button';
 import * as routes from 'app/routes';
-import cn from 'classnames';
 import add_green_circle from 'common/img/add_green_circle.svg';
 import styles from './Subscription.module.css';
-import { fetchFiltered } from '../../insurance/insuranceContracts/api';
-import { getSprintSubscription } from 'reducers';
 
-export const SubscriptionScreen = ({ subId, subscription }) => {
+export const SubscriptionScreen = (props) => {
   const [showInsuranceLink, setShowInsuranceLink] = useState(false);
-  const [showPlanLink, setShowPlanLink] = useState(false);
   const [sku, setSku] = useState('');
+  const { subId, fetchFilteredInsuranceContracts, contracts, subscription } = props;
 
   useEffect(() => {
-    fetchFiltered({subscription: subId}).then(res => {
-      if (res.data && res.data.count > 0) {
-        setShowInsuranceLink(false);
-        setShowPlanLink(false)
-      } else {
-        setShowInsuranceLink(true);
-      }
-    });
-  }, [subId]);
+    subId && fetchFilteredInsuranceContracts({subscription: subId});
+  }, []);
 
   useEffect(() => {
+    if (!contracts) return;
+    if (contracts.length > 0) {
+      setShowInsuranceLink(false)
+    } else {
+      setShowInsuranceLink(true)
+    }
+
     if (!subscription) return;
-
     setSku(subscription.device_specs);
 
     const subStatus = subscription.att_status || subscription.sprint_status;
 
     if (subStatus === 'active') {
-      setShowPlanLink(false)
+      setShowInsuranceLink(false)
     } else {
-      setShowPlanLink(true)
+      setShowInsuranceLink(true)
     }
-  }, [subscription]);
+  }, [contracts]);
 
   return (
     <div className="Subscription">
       <SubscriptionSwitcher sprintSubId={subId} sprintRoute={routes.sprintSubscription} attRoute={routes.attSubscription} />
       <div className={styles.linkList}>
-        <Link
-          className={cn(
-          styles.subscriptionLink,
-          showInsuranceLink ? '' : styles.hidden
-        )}
-          to={routes.sprintInsurance(subId)}
-        >
-          <Box>
-            <img src={add_green_circle} />
-            Insurance
-          </Box>
-        </Link>
-        <Link
-          className={cn(showPlanLink ? '' : styles.hidden)}
-          to={routes.sprintInsurancePlan(subId, sku)}
-        >
-          <div className={styles.buttonChoose}>
-            <Button>Choose insurance plan</Button>
-          </div>
-        </Link>
+        {
+          showInsuranceLink ?
+            <Link className={styles.subscriptionLink} to={routes.sprintInsurancePlan(subId, sku)}>
+              <Box>
+                <img src={add_green_circle} />
+                Insurance
+              </Box>
+            </Link>
+            : null
+        }
       </div>
     </div>
   )
 };
 
 const mapStateToProps = (state, { subId }) => ({
-  subscription: getSprintSubscription(state, subId)
+  subscription: getSprintSubscription(state, subId),
+  contracts: getFilteredInsuranceContracts(state, {subscription: subId})
 });
 
+const mapDispatchToProps = {
+  fetchFilteredInsuranceContracts
+};
+
 export default connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(SubscriptionScreen)
 
