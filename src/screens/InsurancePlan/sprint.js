1fef2a12a825fb71e4da05ac521b7b76a1d92142
Roman Afanasyev <afanasyev.r@yandex.ru>
2019-12-23 01:10:33 +0300

    Plans page WIP

--- src/screens/InsurancePlan/sprint.js
+++ src/screens/InsurancePlan/sprint.js
@@ -1,9 +1,90 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+import cn from 'classnames';
+import * as routes from 'app/routes';
+import { fetchValue } from '../../insurance/insurancePlans/actions';
+import Back from 'common/Back';
+import Link from 'common/Link';
+import checkmark from './checkmark.svg'
 import styles from './InsurancePlan.module.css';
 
-const SprintInsurancePlan = ({}) => (
-  <div>
-  </div>
-)
+const SprintInsurancePlan = (props) => {
+  console.log('prprp', props);
+  const [planList, setPlanList] = useState([]);
+  const { plans, fetchValue, params } = props;
 
-export default SprintInsurancePlan;
+  useEffect(() => {
+    fetchValue('sprint')
+  }, [])
+
+  useEffect(() => {
+    if (plans) {
+      setPlanList(Object.values(plans))
+      console.log(plans);
+    }
+  }, [plans]);
+
+  const {subId} = props
+  return (
+    <div>
+      <div className={styles.Back}>
+        <Back to={routes.sprintSubscription(subId)} />
+      </div>
+      <h1>
+        Select an insurance plan
+      </h1>
+      <div className={styles.Plans}>
+        {planList.map(plan => (
+          <div className={cn(
+            styles.Card,
+            plan.id === 2 ? styles.CardMain : null
+          )}>
+            <div className={styles.CardHeader}>
+              <h2 className={styles.CardName}>{plan.name}</h2>
+              <div className={styles.CardPrice}>
+                Starting at <span className={styles.CardPriceValue}>{`$${plan.price}`}</span>
+              </div>
+              <a className={styles.CardTermsLink} href="#">Terms, fees and more info</a>
+              <Link to={routes.attInsuranceConfirm(subId, params.sku, plan.id)}>
+                <button className={styles.CardButton}>Select</button>
+              </Link>
+            </div>
+            <div className={styles.CardBody}>
+              Protects your device against
+              <ul className={styles.CardList}>
+                <li><img src={checkmark} alt=""/>Accidental damage</li>
+                <li><img src={checkmark} alt=""/>Loss and theft</li>
+                <li><img src={checkmark} alt=""/>Malfunction</li>
+                <li><img src={checkmark} alt=""/>Water damage</li>
+                <li><img src={checkmark} alt=""/>Broken screens</li>
+              </ul>
+            </div>
+            <div>
+              <ul>
+                <li>Repair deductible: ${plan.repair_deductible}</li>
+                <li>Replacement deductible: ${plan.replacement_deductible}</li>
+              </ul>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state, ownProps) => {
+  console.log(state, ownProps);
+  const plans = state.insurancePlans.resources;
+  const params = state.routing.params;
+  return { plans, params }
+};
+
+const mapDispatchToProps = {
+  fetchValue
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SprintInsurancePlan)
+
